3d039b79806079e7309defb08602d162a9c3e200
callmesheep <dungntth2206013@fpt.edu.vn>
2024-03-18 22:31:42 +0700

    update

---
+++ config-overrides.js
@@ -0,0 +1,14 @@
+module.exports = function override (config, env) {
+    console.log('override...')
+    let loaders = config.resolve
+    loaders.fallback = {
+        "fs": false,
+        "http": require.resolve("stream-http"),
+        "zlib": require.resolve("browserify-zlib") ,
+        "path": require.resolve("path-browserify"),
+        "crypto": require.resolve("crypto-browserify"),
+        "vm":require.resolve("vm-browserify")
+    }
+    
+    return config
+}
